f213872792af4ab2fe9e0c3c1e0293cf06977959
chipsettsr <chipsettsr@gmail.com>
2023-11-19 16:29:52 +0400

    Update TestGreeter.java
    test manual failure

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,3 +42,10 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
+   @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Sad";
+    asserttrue(false);
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+}
